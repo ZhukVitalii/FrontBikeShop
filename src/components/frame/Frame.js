a980c3204734d748627d6726b9d207847074012f
ZhukVitalii <zhukvitaliis@gmail.com>
2019-12-04 21:10:25 +0200

    added redux

--- src/components/frame/Frame.js
+++ src/components/frame/Frame.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { createStore } from "redux";
 
-function updateState(bikeTypeId){
-    this.setState({bikeTypeId:bikeTypeId})
-}
+
+const store = createStore();
 class Frames extends React.Component {
+
+
 
     state = {
         frames: [],
